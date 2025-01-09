717c676a04d600645f9c590482a42abc752e6138
Liam Butcher <liam_butcher@hotmail.co.uk>
2025-01-09 18:05:54 +0000

    movies list

---
+++ app/models/bookmark.rb
@@ -0,0 +1,10 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :movie, presence: true
+  validates :list, presence: true
+  validates :comment, length: { minimum: 6 }, allow_blank: true
+
+  validates_uniqueness_of :movie_id, scope: :list_id
+end
