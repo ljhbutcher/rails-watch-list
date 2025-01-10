daa0c7651a0e3dfb5f370174978157525d21af4c
Liam Butcher <liam_butcher@hotmail.co.uk>
2025-01-10 12:40:08 +0000

    watch list done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-
-
-  resources :lists, only: [:index, :show, :new, :create] do
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
     resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
   end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
--- db/migrate/20250109150814_create_bookmarks.rb
+++ db/migrate/20250109150814_create_bookmarks.rb
@@ -1,9 +1,9 @@
-class CreateBookmarks < ActiveRecord::Migration[7.1]
+class CreateBookmarks < ActiveRecord::Migration[7.0]
   def change
     create_table :bookmarks do |t|
+      t.text :comment
       t.references :movie, null: false, foreign_key: true
       t.references :list, null: false, foreign_key: true
-      t.string :comment
 
       t.timestamps
     end
